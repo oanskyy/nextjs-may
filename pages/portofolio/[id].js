a1af1d0764448c8a595e42b3b51d78ddc9dbbaf1
oanskyy <oana.anghelachi@gmail.com>
2023-05-08 18:50:53 +0800

    feat: extract dynamic data with file based routing

--- pages/portofolio/[id].js
+++ pages/portofolio/[id].js
@@ -1,4 +1,12 @@
+// react hook
+import { useRouter } from "next/router"
+
 export default function PortofolioProjectPage() {
+	// router object, exposes various data/methods : the pathname
+	const router = useRouter()
+	console.log(router.pathname)
+	console.log(router.query)
+
 	return (
 		<div>
 			<h1>The portofolio project page</h1>
