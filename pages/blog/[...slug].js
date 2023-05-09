d67473ae43975f3fc45814d6d982b35e399fe7a0
oanskyy <oana.anghelachi@gmail.com>
2023-05-09 20:58:27 +0800

    feat: add catch-all routes

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,17 @@
+// react hook
+import { useRouter } from "next/router"
+
+function BlogPage() {
+	// router object, exposes various data/methods : the pathname
+	const router = useRouter()
+	console.log(router.pathname)
+	console.log(router.query)
+
+	return (
+		<div>
+			<h1>The blog page</h1>
+		</div>
+	)
+}
+
+export default BlogPage
