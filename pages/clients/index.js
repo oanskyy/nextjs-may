b11fc0951896e477251fab12f84317242920aedb
oanskyy <oana.anghelachi@gmail.com>
2023-05-10 15:41:10 +0800

    feat: navigate to dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,15 +1,28 @@
 // react hook
 // import { useRouter } from "next/router"
+import Link from "next/link"
 
 export default function ClientsPage() {
 	// router object, exposes various data/methods : the pathname
 	// const router = useRouter()
 	// console.log(router.pathname)
+
 	// console.log(router.query)
+	const clients = [
+		{ id: "oan", name: "oan art" },
+		{ id: "jas", name: "jas min" }
+	]
 
 	return (
 		<div>
 			<h1>The clients page</h1>
+			<ul>
+				{clients.map(client => (
+					<li key={client.id}>
+						<Link href={`/clients/${client.id}`}>{client.name}</Link>
+					</li>
+				))}
+			</ul>
 		</div>
 	)
 }
