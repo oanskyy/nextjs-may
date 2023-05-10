c76a4d616e0b569641122eeaefe4a89bd7e8e061
oanskyy <oana.anghelachi@gmail.com>
2023-05-10 15:26:27 +0800

    feat: navigating with the link component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Link from "next/link"
+
 export default function HomePage() {
 	return (
 		<div>
 			<h1>The home page</h1>
+			<ul>
+				<li>
+					<Link href='/blog'>Blog</Link>
+				</li>
+				<li><Link href='/clients'>Clients</Link></li>
+			</ul>
 		</div>
 	)
 }
